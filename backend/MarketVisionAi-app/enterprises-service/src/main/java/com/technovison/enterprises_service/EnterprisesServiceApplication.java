ffaaa04bcc374db458adda0a2804abbe88e4a2ff
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-04-07 17:30:08 -0300

    Second Commit: Project Structure

---
+++ backend/MarketVisionAi-app/enterprises-service/src/main/java/com/technovison/enterprises_service/EnterprisesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.technovison.enterprises_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterprisesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterprisesServiceApplication.class, args);
+	}
+
+}
