8b9d6bec1abc62a8818fd4451a32c666955fc552
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-04-07 17:30:08 -0300

    Second Commit: Project Structure

---
+++ backend/MarketVisionAi-app/enterprises-service/src/test/java/com/technovison/enterprises_service/EnterprisesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technovison.enterprises_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterprisesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
