7ac7ff5950963bbece676d44eee1ade49c8ff60f
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-04-07 17:54:11 -0300

    fix: eliminada carpeta duplicada y renombrado backend

---
+++ backend/predictions-service/src/test/java/com/technovison/predictions_service/PredictionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technovison.predictions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
